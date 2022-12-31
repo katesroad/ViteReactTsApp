8aea2c1f0c110b26837304ba29577ed211adc3fe
Kate <3837437+katesroad@users.noreply.github.com>
2022-12-31 09:34:16 -0500

    Update .eslintrc setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,77 +1,130 @@
-/* eslint-disable no-undef */
+
+
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
-  'ignorePatterns': ['./storybook'],
-  'overrides': [],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
   },
-  'plugins': ['react', '@typescript-eslint'],
-  'rules': {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+  ],
+  ignorePatterns: [
+    './storybook',
+    'vite.*.ts',
+    'vite-env.d.ts',
+    './vite'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-import-order',
+    'eslint-plugin-import',
+    'eslint-plugin-react-hooks',
+    'react',
+  ],
+  rules: {
     'additional-rule': 'off',
     'arrow-body-style': 'off',
     'arrow-parens': ['error', 'always'],
     'eol-last': ['error', 'always'],
-    'import/newline-after-import': ['error', {
-      'count': 1
-    }],
-    'max-len': ['error', {
-      'code': 120
-    }],
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1,
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+      },
+    ],
     'multiline-ternary': ['error', 'always'],
     'no-console': 'error',
-    'import/order': ['error', {
-      'groups': ['external', 'builtin', 'internal', 'sibling', 'parent', 'index']
-    }],
-    'indent': ['error', 2],
-    'padding-line-between-statements': ['error', {
-      'blankLine': 'always',
-      'prev': 'while',
-      'next': '*'
-    }, {
-      'blankLine': 'always',
-      'prev': 'if',
-      'next': '*'
-    }, {
-      'blankLine': 'always',
-      'prev': '*',
-      'next': 'if'
-    }, {
-      'blankLine': 'always',
-      'prev': '*',
-      'next': 'return'
-    }, {
-      'blankLine': 'always',
-      'prev': '*',
-      'next': 'while'
-    }, {
-      'blankLine': 'always',
-      'prev': '*',
-      'next': 'function'
-    }, {
-      'blankLine': 'always',
-      'prev': '*',
-      'next': 'for'
-    }, {
-      'blankLine': 'always',
-      'prev': 'for',
-      'next': '*'
-    }],
-    'quotes': [2, 'single', {
-      'avoidEscape': true
-    }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+        ],
+      },
+    ],
+    indent: ['error', 2],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: 'while',
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: 'if',
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'if',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'while',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'function',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'for',
+      },
+      {
+        blankLine: 'always',
+        prev: 'for',
+        next: '*',
+      },
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/no-array-index-key': 'error',
     'react/jsx-uses-react': 'error',
     'react/jsx-max-props-per-line': 'error',
     'react/jsx-sort-props': 'error',
-    'semi': ['error', 'always']
-  }
+    semi: ['error', 'always'],
+  },
 };
